a2c6089cc49754cea025fede8c03fb109e5913cb
Abdullah Jaswal <s4011121@student.rmit.edu.au>
2023-11-08 14:00:16 +1100

    update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,18 +3,36 @@ import Section1 from "@/components/pages/landing/section1";
 import { GetServerSidePropsContext } from "next";
 import { Session, getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
+import Head from "next/head";
 
 type Props = {
   session: Session | null;
 };
+``;
 
 export default function Home(props: Props) {
   return (
-    <div className="flex flex-col gap-10">
-      <Hero session={props.session} />
+    <>
+      <Head>
+        <title>Home - FundsNav</title>
+        <meta
+          name="description"
+          content="FundsNav is a platform that helps you track your mutual fund investments and provides you with insights to help you make better investment decisions."
+        />
+        <meta
+          name="keywords"
+          content="mutual funds, mutual fund, mutual fund investments, mutual fund investment, mutual fund returns, mutual fund investment returns, mutual fund investment returns calculator, mutual fund investment returns calculator india, mutual fund investment returns calculator excel, mutual fund investment returns calculator excel india, mutual fund investment returns calculator excel sheet, mutual fund investment returns calculator excel sheet india, mutual fund investment returns calculator excel sheet download, mutual fund investment returns calculator excel sheet free download, mutual fund investment returns calculator excel sheet with sip, mutual fund investment returns calculator excel sheet with sip india, mutual fund investment returns calculator excel sheet with sip download, mutual fund investment returns calculator excel sheet with sip free download"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <Section1 />
-    </div>
+      <div className="flex flex-col gap-10">
+        <Hero session={props.session} />
+
+        <Section1 />
+      </div>
+    </>
   );
 }
 
